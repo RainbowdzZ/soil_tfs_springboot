470c59b27f23b08f586552cf107da38570b54d33
Minami <672716729@qq.com>
2023-11-25 10:19:18 +0800

    2023.11.25上午添加注释

--- ruoyi-wx/src/main/java/com/ruoyi/wx/domain/dto/HomeDto.java
+++ ruoyi-wx/src/main/java/com/ruoyi/wx/domain/dto/HomeDto.java
@@ -4,16 +4,24 @@ import lombok.Data;
 
 import java.util.List;
 
+/**
+ * 首页数据对象
+ */
 @Data
 public class HomeDto {
     @Data
     public static class DataCount {
+        //今日交易订单
         private int orderCount;
+        //未检测土壤
         private int utSoilCount;
+        //用户数量
         private int userCount;
+        //反馈数量
         private int feedbackCount;
     }
 
+    //折线图数据
     @Data
     public static class DateCount {
         private String orderDate;
